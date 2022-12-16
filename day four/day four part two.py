a99a2371a1440a66f8f2516d9cc4b5b0d57d9c1b
dovagneau <mathis.coiraton@gmail.com>
2022-12-16 15:27:20 +0100

    part two

---
+++ day four/day four part two.py
@@ -0,0 +1,11 @@
+txt_input = open("day four\input.txt","r")
+pairs=txt_input.readlines()
+r=0
+for pair in pairs:
+    e1, e2 = pair.split(",")
+    start1, end1 = map(int, e1.split("-"))
+    start2, end2 = map(int, e2.split("-"))
+    if any(x in range(start2, end2+1) for x in range(start1, end1+1)) or any(x in range(start1, end1+1) for x in range(start2, end2+1)):
+        r += 1
+
+print(r)
