8735d31eed8ef5f1a68f3bb3f9743f154000a22b
dovagneau <mathis.coiraton@gmail.com>
2022-12-16 20:42:03 +0100

    part two done

---
+++ day five/day five part two.py
@@ -0,0 +1,33 @@
+txt_input=open("day five\input.txt","r")
+procedures=[]
+stacks=[
+    ["J","Z","G","V","T","D","B","N"],
+    ["F","P","W","D","M","R","S"],
+    ["Z","S","R","C","V"],
+    ["G","H","P","Z","J","T","R"],
+    ["F","Q","Z","D","N","J","C","T"],
+    ["M","F","S","G","W","P","V","N"],
+    ["Q","P","B","V","C","G"],
+    ["N","P","B","Z"],
+    ["J","P","W"],
+]
+i=1
+for lines in txt_input.readlines():
+    if i>=11 :
+        procedures.append(lines)
+    i+=1
+for procedure in procedures:
+    nbs=int(procedure.split(" from ")[0].strip().split(" ")[-1])
+    fr=int(procedure.split(" from ")[1].split(" to ")[0].strip())
+    to=int(procedure.split(" from ")[1].split(" to ")[1].strip())
+    for j in range(0,nbs):
+        if(nbs==1 and len(stacks[fr-1])>0):
+            stacks[to-1].insert(0,stacks[fr-1].pop(0))
+        elif(nbs>1 and len(stacks[fr-1])>0):
+            stacks[to-1].insert(0,stacks[fr-1].pop(nbs-j-1))
+#affichage :
+r=""
+for stack in stacks:
+    print(str(stack[0]))
+    r+=stack[0]
+print(r)
