24d8dba9b90e88c7064e9e030086ea3c8c27c5cd
dovagneau <mathis.coiraton@gmail.com>
2022-12-16 23:09:54 +0100

    day six part two

---
+++ day six/day six part two.py
@@ -0,0 +1,18 @@
+txt_input=open("day six\input.txt","r")
+datastream=txt_input.readline()
+last14char=[]
+i=0
+markerfound=False
+while(not markerfound and i<len(datastream)):
+    if(len(last14char)<14):
+        last14char.append(datastream[i])
+    elif(len(set(last14char)) == len(last14char)): 
+        markerfound=True
+    else :
+        del last14char[0]
+        last14char.append(datastream[i])
+
+    i+=1
+print(last14char)
+print(markerfound)
+print(i-1)
